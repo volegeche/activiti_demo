23664d7688d22fb560fab464c5b9abc68f11f06f
huangyewei <yeweihuang@edayun.cn>
2019-12-30 01:33:14 +0800

    表查询

---
+++ file/tableQuery.sql
@@ -0,0 +1,22 @@
+#ge =  general
+select * from ACT_GE_PROPERTY; #系统属性
+select * from ACT_GE_BYTEARRAY; #大属性值
+#re = repository
+select * from ACT_RE_MODEL;#设计模型表
+select * from ACT_RE_DEPLOYMENT;#部署信息表
+select * from ACT_RE_PROCDEF;#流程定义表
+#ru = runtime
+select * from ACT_RU_EXECUTION;#执行实例
+select * from ACT_RU_TASK;#执行任务
+select * from ACT_RU_VARIABLE;#执行中变量表
+select * from ACT_RU_IDENTITYLINK;#人员关系
+
+#hi = history
+select * from ACT_HI_ACTINST;# 历史活动
+select * from ACT_HI_TASKINST;#历史任务
+select * from ACT_HI_PROCINST;#历史流程实例
+select * from ACT_HI_COMMENT;#审批意见
+select * from ACT_HI_IDENTITYLINK;
+select * from ACT_HI_VARINST;
+
+
